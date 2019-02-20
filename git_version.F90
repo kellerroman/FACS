55f995d74ce90a76a4c1caf3fc3f01b9e5d9980d
roman <roman@rkeller.net>
2019-02-20 21:12:47 +0100

    CMAKE working for tools

--- git_version.F90
+++
@@ -1,5 +0,0 @@
-module git_version_module
-    implicit none
-    character(len=*), parameter :: GIT_VERSION = "36cb2b9d34a00491cdf6ee5c01cbb44fac9c6a4e-dirty"
-    character(len=*), parameter :: GIT_DATE    = "Sun Jan 20 20:58:11 2019"
-end module git_version_module
