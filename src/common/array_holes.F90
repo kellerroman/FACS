63c6f7b20c50396d7706fa078cd7d1631a2f91f0
roman <roman@rkeller.net>
2019-09-18 23:48:32 +0200

    holes must be deleted if they are at the end of the array

--- src/common/array_holes.F90
+++ src/common/array_holes.F90
@@ -5,7 +5,7 @@ integer, parameter :: N_HOLES_MAX = 1000
 type :: holes
     private 
     !integer :: MaxArray
-    integer :: lastEntry                          ! Position where to add when no more holes 
+    integer, public :: lastEntry                          ! Position where to add when no more holes 
     integer, allocatable :: holes(:)
     integer, public :: nEntry                             ! Number of Entries (not necessary continues
     integer, public :: nHoles                             ! Number of Holes in the Array
@@ -46,15 +46,45 @@ subroutine remove_entry(this,id)
     Class(holes), intent(inout) :: this
     integer, intent(in) :: id
 
+    integer :: mypos, t
     this % nEntry = this % nEntry - 1
     if (id == this % lastEntry) then
+        ! write(*,*) "Deleting last Entry: ",id
+        mypos = 0
+        t = id - 1
         this % lastEntry = this % lastEntry - 1
+        ! check if end of array is all holes
+        do 
+            if (this % holes(mypos+1) == t .and. mypos < this % nHoles) then
+                ! write(*,*) "Deleting Hole : ",t,"@",mypos+1
+                t = t - 1
+                mypos = mypos + 1
+            else 
+                this % lastEntry = this % lastEntry - mypos 
+                this % holes(1:this % nholes-mypos) = this % holes(mypos+1:this % nholes)
+                this % nHoles = this % nHoles - mypos
+                exit
+            end if
+        end do
     else if (id > this % lastEntry) then
         write(*,*) "Array Value does not exist"
         stop 1
     else
         this % nHoles = this % nHoles + 1
         this % holes(this % nHoles) = id
+        mypos = this % nHoles
+         ! sort the holes by larg at last
+        do 
+            if (mypos < 2) exit
+            t = this % holes(mypos - 1)
+            if (id > t ) then
+                this % holes(mypos) =  t
+                this % holes(mypos - 1) = id
+                mypos = mypos - 1
+            else 
+                exit
+            end if
+        end do
     end if
 
 end subroutine remove_entry
